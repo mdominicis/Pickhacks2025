69454f108807dc4c53ec3d1262a32ef614f6a2a8
Ben <benhartmann26@gmail.com>
2025-03-02 00:15:45 -0600

    improved example

--- talkingSnom.py
+++ talkingSnom.py
@@ -6,7 +6,9 @@ from poke_env import AccountConfiguration, ServerConfiguration
 from poke_env.player import Player
 from typing import Awaitable
 
-class MaxDamagePlayer(Player):    
+class MaxDamagePlayer(Player):
+
+    #IGNORE THIS    
     def choose_move(self, battle):
         if battle.available_moves:
             best_move = max(battle.available_moves, key=lambda move: move.base_power)
@@ -14,12 +16,14 @@ class MaxDamagePlayer(Player):
         else:
             return self.choose_random_move(battle)
 
+    #Function changed to include trash_talk
     async def _handle_battle_request(
         self,
         battle,
         from_teampreview_request: bool = False,
         maybe_default_order: bool = False,
     ):
+        #IMPORTANT: COMMAND BELOW IS ADDED TO FUNCTION
         await self.trash_talk(battle)
         if maybe_default_order and random.random() < self.DEFAULT_CHOICE_CHANCE:
             message = self.choose_default_move().message
@@ -36,22 +40,35 @@ class MaxDamagePlayer(Player):
         if not battle._wait:
             await self.ps_client.send_message(message, battle.battle_tag)
 
+    #Basic trash talk
     async def trash_talk(self,battle):
-        print("fish")
-        message = chat.send_message("I have 2 dogs in my house.")
+        last_turn = battle.observations[battle._turn-1]
+        end_prompt = ". Do this in one paragraph and only use dialogue, do not describe your actions. do not speak out of character."
+        if battle._turn == 1:
+            #FIRST TURN MESSAGE
+            message = chat.send_message("Pretend you are a Pokemon trainer. Challenge me to a pokemon battle. Your first pokemon is "+battle.active_pokemon._name+end_prompt)
+        elif battle.opponent_active_pokemon.is_terastallized != last_turn.opponent_active_pokemon.is_terastallized:
+            #HUMAN TERA
+            message = chat.send_message("Uh oh, your opponent just terastallized! This changes their pokemon's type. say your reaction"+end_prompt)
+        else:
+            return 0
         await self.ps_client.send_message(message.text, battle.battle_tag)
 
 
 async def main():
+    #Uses my testing account
     player = MaxDamagePlayer(
         account_configuration=AccountConfiguration("talking snom", "pickhacks2025"),
         )
 
+    #Challenges me
     await player.send_challenges("Elytrix", n_challenges=1)
 
 
 
 if __name__ == "__main__":
-    client = genai.Client(api_key="AIzaSyALnRlIglNFd5kEhJadfP_rD4Qmmx45qmo")
+    #Sets api key and creates a "chat" which trash_talk will use
+    client = genai.Client(api_key="AIzaSyDY50hm-IIwxEgH9hvNEk9ews7Het3v1Qw")
     chat = client.chats.create(model="gemini-2.0-flash")
+    
     asyncio.get_event_loop().run_until_complete(main())
